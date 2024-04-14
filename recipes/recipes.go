8bcdcd4373c484439feb63741503e2d3c1775562
Ivan Ignatov <ivanign04@gmail.com>
2024-04-14 16:10:27 +1200

    Refactor functions for finding ingredient and instructions sections into single general function.

--- recipes/recipes.go
+++ recipes/recipes.go
@@ -37,61 +37,45 @@ func (subResult *SubResult) appendCandidates(candidates [][]string) {
 	subResult.Candidates = append(subResult.Candidates, candidates...)
 }
 
+func findSection(doc *goquery.Document, priorityElementType string, keywords []string) (SubResult, error) {
+	result := SubResult{}
+
+	possiblePriorityElementSelections := findByClassOrIdContains(doc, priorityElementType, keywords[0])
+
+	if len(possiblePriorityElementSelections) > 0 {
+		result.DiscoveredVia = priorityElementType
+
+		var candidates [][]string
+
+		switch priorityElementType {
+		case "ul":
+			candidates = ulToCandidates(possiblePriorityElementSelections)
+		case "ol":
+			candidates = ulToCandidates(possiblePriorityElementSelections)
+			// TODO: other types of priority elements?
+		}
+
+		result.appendCandidates(candidates)
+		return result, nil
+	}
+
+	result.DiscoveredVia = "*"
+	possibleRelevantElementSelections := findByClassOrIdContains(doc, "*", instructionsKeywords[0])
+
+	if len(possibleRelevantElementSelections) == 0 {
+		return result, nil
+	}
+
+	candidates := textFromDeepestLastOfType(possibleRelevantElementSelections)
+	result.appendCandidates(candidates)
+
+	return result, nil
+}
+
 type RecipePullResult struct {
 	Url          string
 	Ingredients  SubResult
 	Instructions SubResult
-}
-
-func findIngredients(doc *goquery.Document) (SubResult, error) {
-	result := SubResult{}
-
-	result.DiscoveredVia = "ul"
-	possibleIngredientUlsByClassOrId := findByClassOrIdContains(doc, "ul", ingredientsKeyword)
-
-	if len(possibleIngredientUlsByClassOrId) > 0 {
-		candidates := ulToCandidates(possibleIngredientUlsByClassOrId)
-		result.appendCandidates(candidates)
-		return result, nil
-	}
-
-	result.DiscoveredVia = "*"
-	possibleIngredientsElements := findByClassOrIdContains(doc, "*", ingredientsKeyword)
-
-	if len(possibleIngredientsElements) == 0 {
-		return result, nil
-	}
-
-	candidates := textFromDeepestLastOfType(possibleIngredientsElements)
-	result.appendCandidates(candidates)
-
-	return result, nil
-}
-
-func findInstructions(doc *goquery.Document) (SubResult, error) {
-	result := SubResult{}
-
-	result.DiscoveredVia = "ol"
-
-	possibleInstructionsOls := findByClassOrIdContains(doc, "ol", instructionsKeywords[0])
-
-	if len(possibleInstructionsOls) > 0 {
-		candidates := ulToCandidates(possibleInstructionsOls)
-		result.appendCandidates(candidates)
-		return result, nil
-	}
-
-	result.DiscoveredVia = "*"
-	possibleInstructionsElements := findByClassOrIdContains(doc, "*", instructionsKeywords[0])
-
-	if len(possibleInstructionsElements) == 0 {
-		return result, nil
-	}
-
-	candidates := textFromDeepestLastOfType(possibleInstructionsElements)
-	result.appendCandidates(candidates)
-
-	return result, nil
 }
 
 func PullRecipe(url string) (RecipePullResult, error) {
@@ -126,14 +110,14 @@ func PullRecipe(url string) (RecipePullResult, error) {
 
 	wg.Add(1)
 	go func() {
-		ingredientsResult, err := findIngredients(doc)
+		ingredientsResult, err := findSection(doc, "ul", []string{ingredientsKeyword})
 		ch <- SubresultChannelOuput{SubResult: ingredientsResult, Error: err, Type: "ingredients"}
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
-		instructionsResult, err := findInstructions(doc)
+		instructionsResult, err := findSection(doc, "ol", instructionsKeywords)
 		ch <- SubresultChannelOuput{SubResult: instructionsResult, Error: err, Type: "instructions"}
 		wg.Done()
 	}()
